7dff6571cc9abedb1455af7456e6cf790f10eaaf
Max de Krieger <maxdekrieger@gmail.com>
2023-01-05 10:19:15 +0100

    complete day 13 in python

--- day13_python/part1.py
+++ day13_python/part1.py
@@ -3,8 +3,8 @@ import os
 from typing import Tuple
 
 def main():
-    # input_name = 'data/input.txt'
-    input_name = 'data/input-test.txt'
+    input_name = 'data/input.txt'
+    # input_name = 'data/input-test.txt'
     input_path = os.path.join(os.path.dirname(__file__), input_name)
 
     with open(input_path, 'r') as file:
@@ -13,24 +13,32 @@ def main():
         sum = 0
 
         index = 1
-        for line in file:
+        for linenumber, line in enumerate(file):
+            # print(f"{linenumber}: {line.strip()}")
             if left is None:
                 left = create_packet(line.strip())[0]
             elif right is None:
                 right = create_packet(line.strip())[0]
             else: 
                 # Calculate if its correct
+                result = left.compare(right)
+                # print(result)
+                if result == 1:
+                    print(f"Pair {index} is in the right order")
+                    sum += index
+
                 # at the end
                 left = None
                 right = None
                 index += 1
+        print(sum)
 
 def create_packet(text: str) -> Tuple[Packet, str]:
     packet = Packet()
 
     if text[0] == '[':
         text = text[1:]
-    elif text[0].isdigit() and text[1].isdigit():
+    elif len(text) >= 2 and text[0].isdigit() and text[1].isdigit():
         packet.literal = int(text[:2])
         return (packet, text[2:])
     elif text[0].isdigit():
@@ -45,7 +53,7 @@ def create_packet(text: str) -> Tuple[Packet, str]:
         else:
             (subpacket, text) = create_packet(text)
             packet.subpackets.append(subpacket)
-            print(text)
+            # print(text)
 
     return (packet, text)
 
@@ -54,6 +62,50 @@ class Packet:
         self.literal: int = None
         self.subpackets: list[Packet] = list()
         pass
+
+    def is_literal(self) -> bool:
+        return self.literal is not None
+
+    def to_string(self) -> str:
+        if self.is_literal():
+            return str(self.literal)
+        else:
+            subpackets_str = list(map(lambda s : s.to_string(), self.subpackets))
+            return f"[{','.join(subpackets_str)}]"
+
+    def compare(self, other : Packet) -> int:
+        # print(f"comparing {self.to_string()} to {other.to_string()}")
+        if self.is_literal() and other.is_literal():
+            if self.literal == other.literal:
+                return 0
+            elif self.literal < other.literal:
+                return 1
+            elif self.literal > other.literal:
+                # print(f"Not in the right order: {self.literal} > {other.literal}")
+                return -1
+        elif self.is_literal():
+            self.subpackets.append(create_packet(f'{self.literal}')[0])
+            self.literal = None
+        elif other.is_literal():
+            other.subpackets.append(create_packet(f'{other.literal}')[0])
+            other.literal = None
+
+        for idx in range(len(self.subpackets)):
+            if idx > (len(other.subpackets) - 1):
+                # print(f"Not in the right order: left has more subpackets than right")
+                return -1
+
+            selfsub = self.subpackets[idx]
+            othersub = other.subpackets[idx]
+
+            subresult = selfsub.compare(othersub)
+            if subresult != 0:
+                return subresult
+
+        if len(self.subpackets) < len(other.subpackets):
+            return 1
+        else:
+            return 0
 
 if __name__ == "__main__":
     main()
--- day13_python/part2.py
+++ day13_python/part2.py
@@ -1,79 +1,114 @@
 from __future__ import annotations
 import os
-import string
+from typing import Tuple
 
 def main():
     input_name = 'data/input.txt'
     # input_name = 'data/input-test.txt'
     input_path = os.path.join(os.path.dirname(__file__), input_name)
 
-    grid: list[list[Square]] = list()
-    start: Square = None
-    end: Square = None
-
     with open(input_path, 'r') as file:
-        for y, line in enumerate(file):
-            row: list[Square] = list()
-            for x, c in enumerate(line.strip()):
-                s = Square(x, y, c)
-                if c == 'S':
-                    start = s
-                elif c == 'E':
-                    end = s
-                row.append(s)
-            grid.append(row)
 
-    for row in grid:
-        for s in row:
-            s.find_and_add_adjacent(grid)
+        packets: list[Packet] = list()
+        divider_2 = create_packet('[[2]]')[0]
+        divider_6 = create_packet('[[6]]')[0]
+        packets.append(divider_2)
+        packets.append(divider_6)
 
-    # flatten grid into set
-    unvisited = set([item for sublist in grid for item in sublist])
-    start.distance = 0
-    while len(unvisited) > 0:
-        u = min(unvisited, key=lambda x: x.distance)
-        unvisited.remove(u)
-        for v in u.adjacent.intersection(unvisited):
-            alt = u.distance + 1
-            if alt < v.distance:
-                v.distance = alt
-                v.previous = u
+        for line in file:
+            line = line.strip()
+            if line:
+                p = create_packet(line.strip())[0]
+                packets.append(p)
 
-    print(end.distance)
+        packets.sort()
+        for p in packets:
+            print(p.to_string())
+        
+        print((packets.index(divider_2) + 1) * (packets.index(divider_6) + 1))
 
-class Square:
-    def __init__(self, x: int, y: int, elevation: str) -> None:
-        self.x = x
-        self.y = y
-        elevation = 'a' if elevation == 'S' else elevation
-        elevation = 'z' if elevation == 'E' else elevation
-        self.elevation: int = string.ascii_lowercase.index(elevation)
-        self.adjacent: set[Square] = set()
+def create_packet(text: str) -> Tuple[Packet, str]:
+    packet = Packet()
 
-        self.distance: int = 0 if elevation == 'a' else float('inf')
-        self.previous: Square = None
+    if text[0] == '[':
+        text = text[1:]
+    elif len(text) >= 2 and text[0].isdigit() and text[1].isdigit():
+        packet.literal = int(text[:2])
+        return (packet, text[2:])
+    elif text[0].isdigit():
+        packet.literal = int(text[0])
+        return (packet, text[1:])
 
-    def find_and_add_adjacent(self, grid: list[list[Square]]):
-        #left
-        if self.x > 0:
-            left = grid[self.y][self.x - 1]
-            if left.elevation <= (self.elevation + 1):
-                self.adjacent.add(left)
-        #right
-        if self.x < (len(grid[0]) - 1):
-            right = grid[self.y][self.x + 1]
-            if right.elevation <= (self.elevation + 1):
-                self.adjacent.add(right)
-        #up
-        if self.y > 0:
-            up = grid[self.y - 1][self.x]
-            if up.elevation <= (self.elevation + 1):
-                self.adjacent.add(up)
-        #down
-        if self.y < (len(grid) - 1):
-            down = grid[self.y + 1][self.x]
-            if down.elevation <= (self.elevation + 1):
-                self.adjacent.add(down)
+    while text:
+        if text[0] == ']':
+            return (packet, text[1:])
+        elif text[0] == ',':
+            text = text[1:]
+        else:
+            (subpacket, text) = create_packet(text)
+            packet.subpackets.append(subpacket)
+            # print(text)
+
+    return (packet, text)
+
+class Packet:
+    def __init__(self) -> None:
+        self.literal: int = None
+        self.subpackets: list[Packet] = list()
+        pass
+
+    def __lt__(self, other) -> bool:
+        result = self.compare(other)
+        if result == 1:
+            return True
+        elif result == -1:
+            return False
+        else:
+            raise RuntimeError(f"Comparing {self.to_string()} to {other.to_string()} gave 0")
+
+    def is_literal(self) -> bool:
+        return self.literal is not None
+
+    def to_string(self) -> str:
+        if self.is_literal():
+            return str(self.literal)
+        else:
+            subpackets_str = list(map(lambda s : s.to_string(), self.subpackets))
+            return f"[{','.join(subpackets_str)}]"
+
+    def compare(self, other : Packet) -> int:
+        # print(f"comparing {self.to_string()} to {other.to_string()}")
+        if self.is_literal() and other.is_literal():
+            if self.literal == other.literal:
+                return 0
+            elif self.literal < other.literal:
+                return 1
+            elif self.literal > other.literal:
+                # print(f"Not in the right order: {self.literal} > {other.literal}")
+                return -1
+        elif self.is_literal():
+            self.subpackets.append(create_packet(f'{self.literal}')[0])
+            self.literal = None
+        elif other.is_literal():
+            other.subpackets.append(create_packet(f'{other.literal}')[0])
+            other.literal = None
+
+        for idx in range(len(self.subpackets)):
+            if idx > (len(other.subpackets) - 1):
+                # print(f"Not in the right order: left has more subpackets than right")
+                return -1
+
+            selfsub = self.subpackets[idx]
+            othersub = other.subpackets[idx]
+
+            subresult = selfsub.compare(othersub)
+            if subresult != 0:
+                return subresult
+
+        if len(self.subpackets) < len(other.subpackets):
+            return 1
+        else:
+            return 0
 
 if __name__ == "__main__":
     main()
